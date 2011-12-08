9e9add9cc28f19ad06498c1e8134ce1419fce9c6
alkz <alkz.0x80@gmail.com>
2011-12-08 15:43:03 +0000

    improved install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,38 @@
 
 echo "Compilig..."
+echo "g++ \`nspr-config --cflags --libs\` -g -o /usr/bin/xzHTTPd ./src/*.cpp ./src/*.hpp"
 g++ `nspr-config --cflags --libs` -g -o /usr/bin/xzHTTPd ./src/*.cpp ./src/*.hpp
+echo "Compiled!"
+echo
+echo "Creating user and directories..."
 
 useradd -m xzhttpd
+
 mkdir /home/xzhttpd/htdocs
 mkdir /home/xzhttpd/logs
+mkdir /etc/xzHTTPd/
+cp    ./etc/xzHTTPd.conf /etc/xzHTTPd
 
-cp ./etc/xzHTTPd.conf /etc
-chown xzhttpd:xzhttpd /etc/xzHTTPd.conf
+echo
+echo "Done!"
+echo
+echo "Setting permissions..."
 
+chown -R xzhttpd:xzhttpd /etc/xzHTTPd
 chown xzhttpd:xzhttpd /usr/bin/xzHTTPd
+chown -R xzhttpd:xzhttpd /home/xzhttpd
 
-echo "Installed"
+echo
+echo "--------------------------------------------------------"
+echo
+echo "xzHTTPd Installed! Enjoy it!"
+echo
+echo "bin:               /usr/bin/xzHTTPd" 
+echo "conf:     /etc/xzHTTPd/xzHTTPd.conf" 
+echo "user:                       xzhttpd"
+echo "group:                      xzhttpd"
+echo "htdocs:        /home/xzhttpd/htdocs"
+echo "logs:            /home/xzhttpd/logs"
+echo
+echo "--------------------------------------------------------"
+echo
